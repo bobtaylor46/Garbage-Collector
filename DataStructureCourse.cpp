050c09c428f9ea1a440ffe14d60b0494a1c9a5e9
bobtaylor46 <btsrigaud22@gmail.com>
2026-09-16 09:48:07 +0800

    selection sort

--- DataStructureCourse.cpp
+++ DataStructureCourse.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 class Intlist {
     void add (int x);
     void insertAt (int i, int x);
@@ -5,3 +7,13 @@ class Intlist {
     bool removeAt(int i);
     int size() const;
 };
+//selection sort
+void selectionSort(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        int j = i;
+        for (int k = i + 1; k < n; k++) {
+            if (a[k] < a[j]) j = k;
+        }
+        std::swap(a[i], a[j]);
+    }
+}
